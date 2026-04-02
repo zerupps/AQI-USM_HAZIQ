15cc637d0c118fb717e72af81668d3f93a26df09
zerupps <zerupps@gmail.com>
2026-04-02 13:47:30 +0800

    app.py

--- app.py
+++ app.py
@@ -32,6 +32,13 @@ if live_ref:
     col1.metric("Temperature", f"{live_ref['temperature']} °C")
     col2.metric("Humidity", f"{live_ref['humidity']} %")
     col3.metric("PM 2.5", f"{live_ref['pm2_5']} µg/m³")
+    last_ts = live_ref.get('timestamp', 'Tiada Data')
+    st.caption(f"🕒 Kemaskini Terakhir: {last_ts}")
+    
+    col1, col2, col3 = st.columns(3)
+    col1.metric("Temperature", f"{live_ref['temperature']} °C")
+    col2.metric("Humidity", f"{live_ref['humidity']} %")
+    col3.metric("PM 2.5", f"{live_ref['pm2_5']} µg/m³")
 
 # 3. Tarik Data Sejarah (Untuk Graf)
 history_ref = db.reference('/history').get()
